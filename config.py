c093c1684ff204fd0276943b8ca18cd99cd699a0
= <jonatansan323@gmail.com>
2024-05-30 14:01:27 -0400

    hemos conectado la base de datos

---
+++ config.py
@@ -0,0 +1,3 @@
+#Conexion a la base de datos
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:12345678@127.0.0.1/db_usuarios'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,8 +1,18 @@
+#Import
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import config #importamos el archivo de configuración "config.py"
+
+#Instancias
 app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy (app)
+
+#Rutas
 @app.route('/')
 def inicio():
     return ('Hola esta es la pagina de inicio')
 
+#Run
 if __name__ == '__main__':
     app.run(debug=True)
